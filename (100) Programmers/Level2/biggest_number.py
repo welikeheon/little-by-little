69db1000582a5c15c77745af77b0a9718d83bc4d
pithecuse527 <gihong96@gmail.com>
2021-12-12 11:28:58 +0900

    make the largest number pwith stack

---
+++ (100) Programmers/Level2/biggest_number.py
@@ -0,0 +1,21 @@
+def solution(numbers, k):
+    # stack with integers to be removed from numbers string
+    stack = []
+    
+    # to be returned string size
+    return_size = len(numbers) - k
+    
+    # going for the each number from numbers string -- O(n)
+    for num in numbers:
+        
+        # something inside of stack, and 
+        # there are still numbers to be removed
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+
+        # push the new num
+        stack.append(num)
+    
+    return "".join(stack)[:return_size]
+    
